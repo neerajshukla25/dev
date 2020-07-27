38fb99fc3040ac0e8524e4157ae4652faa14233f
Simplilearn_GitHub <neerajshukla2531@gmail.com>
2020-07-27 21:44:46 +0530

    second commit

--- Clone/activity/main.js
+++ Clone/activity/main.js
@@ -43,3 +43,8 @@ app.on('window-all-closed', () => {
   })
     // v In this file you can include the rest of your app's specific main process
     // code. You can also put them in separate files and require them here.
+    aiishdkjashdkjasdd
+    akjsdnajksd
+    alkksndkjasd
+    aksjdnajksd
+    kajsbdkja
